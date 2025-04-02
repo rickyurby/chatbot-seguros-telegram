cde8ad4d873f6aa39213a476de2e2b8307a81a15
rickyurby <rickyurby@gmail.com>
2025-04-01 18:39:49 -0600

    Add files via upload

---
+++ ping_system.py
@@ -0,0 +1,7 @@
+import requests
+import time
+import os
+
+while True:
+    requests.get("https://chatbot-seguros-telegram.onrender.com")  # Reemplaza con tu URL
+    time.sleep(300)  # Ping cada 5 minutos
